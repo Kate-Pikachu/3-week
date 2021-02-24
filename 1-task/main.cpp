9b7f46e513a4e265db6c909547bb18bf7d8127ba
Kate-Pikachu <pikulina.ev@phystech.edu>
2021-02-25 00:42:32 +0300

    буууп

--- 1-task/main.cpp
+++ 1-task/main.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 #include <array>
+#include<vector>
 #include <set>
 #include <algorithm>
 #include <chrono>
+
 using namespace std;
+using namespace std::chrono;
 
 int main() {
 
@@ -17,20 +20,31 @@ int main() {
 	st.insert(9);
 
 	auto end_1 = steady_clock::now();
-
-	auto delta_1 = duration_cast<milliseconds>(end_2 - begin_2);
-	std::cout << "Time in second case: " << delta_1.count() << " milliseconds" << std::endl;
-
+	auto delta_1 = duration_cast<nanoseconds>(end_1 - begin_1);
+	std::cout << "Time in first case: " << delta_1.count() << " nanoseconds" << std::endl;
+	 
 	
 	auto begin_2 = steady_clock::now();
-
 	array<int,5> arr = { 1,3,5,7,9 };
 	sort(arr.begin(), arr.end());
-
 	auto end_2 = steady_clock::now();
+	auto delta_2 = duration_cast<nanoseconds>(end_2 - begin_2);
+	std::cout << "Time in second case (with array): " << delta_2.count() << " nanoseconds" << std::endl;
 
-	auto delta_2 = duration_cast<milliseconds>(end_2 - begin_2);
-	std::cout << "Time in second case: " << delta_2.count() << " milliseconds" << std::endl;
+
+
+	auto begin_3 = steady_clock::now();
+
+	vector<int> vec = { 1,3,5,7,9 };
+	sort(vec.begin(), vec.end());
+
+	auto end_3 = steady_clock::now();
+
+	auto delta_3 = duration_cast<nanoseconds>(end_3 - begin_3);
+	std::cout << "Time in third case (with vector): " << delta_3.count() << " nanoseconds" << std::endl;
+
+
+
 
 	system("pause");
 	return EXIT_SUCCESS;
