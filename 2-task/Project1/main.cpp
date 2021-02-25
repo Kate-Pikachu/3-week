a74fb8f0aa89db39170c834abc780aed596f3354
Kate-Pikachu <pikulina.ev@phystech.edu>
2021-02-25 13:56:26 +0300

    bup

--- 2-task/Project1/main.cpp
+++ 2-task/Project1/main.cpp
@@ -3,14 +3,19 @@
 using namespace std;
 
 int Hash_Function(int key) {
-	static RandomNumber rnd;
 	float f;
-
-	f = key * rnd.fRandom(); // умножить ключ на случайное число из диапазона 0...1
-	f = f - int(f); // взять дробную часть
-	return 701 * f; 	// возвратить число в диапазоне 0...n-1
+	float x;
+	cout << "enter random number in the range 0 ... 1: ";
+	cin >> x;
+	cout << endl;
+	f = key * x; // multiply the key by a random number in the range 0 ... 1
+	f = f - int(f); // take the fractional part
+	return 101 * f; 	// return a number in the range 0 ... n-1
 }
 
 int main() {
 
+
+	system("pause");
+	return EXIT_SUCCESS;
 }
