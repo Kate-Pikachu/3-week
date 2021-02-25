9ce88f10fb0842a6246d2ffa3a73e92a51195e35
Kate-Pikachu <pikulina.ev@phystech.edu>
2021-02-25 16:14:37 +0300

    commit

--- 2-task/Project1/main.cpp
+++ 2-task/Project1/main.cpp
@@ -1,32 +1,18 @@
 #include <iostream>
-#include <array>
-#include <unordered_set>
 
-using namespace std;
+std::size_t Hash_Function(float key) {
 
-int Hash_Function(float key) {
 	float f;
-	float x;
-	cout << "enter random number in the range 0 ... 1: ";
-	cin >> x;
-	cout << endl;
-	f = key * x; // multiply the key by a random number in the range 0 ... 1
+
+	f = key * 0.57; // multiply the key by a random number in the range 0 ... 1
 	f = f - int(f); // take the fractional part
-	return 6 * f; 	// return a number in the range 0 ... n-1
+	return 2 * f;
 }
 
 int main() {
-	//array<float,3> arr = { 1.345, 2.4, 5.3 };
 
-	unordered_set<float> st;
-	st.insert(1);
-	st.insert(3);
-	st.insert(5);
-	st.insert(7);
-	st.insert(9);
-
-	Hash_Function(st);
-	cout << "end" << endl;
+	float q = 1.23;
+	std::cout << "result: " <<Hash_Function(q) << std::endl;
 
 	system("pause");
 	return EXIT_SUCCESS;
